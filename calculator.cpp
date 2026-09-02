b6d2cb0bb6d0f942b97fc58fb55f4087d687c7b7
schroederconnor <schroederConnor08@gmail.com>
2026-09-02 09:45:49 -0700

    created and started basic version of files. that way i can get it to work then create  the more advanced printings and ifs in the original file once i get it to work

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include "calculator.h"
+
+double add(double val1,double val2)
+{
+    return val1+val2;
+}
+double subtract(double val1,double val2)
+{
+    return val1-val2;
+}
+
+double multiply(double val1,double val2)
+{
+    return val1*val2;
+}
+
+double divide(double val1,double val2)
+{
+    return val1/val2;
+}
+//add.cpp add.h
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include "calculator.h"
+
+int main()
+{
+    double value1, value2, result;
+    string name;
+    cin >> value1 >> value2;
+    cout << value1 << " & " << value2 << endl;
+
+    result = add(value1,value2);
+    cout << name << value1 <<" + " << value2 << " = " << result << endl;
+
+    result = subtract(value1,value2);
+    cout << name << value1 <<" - " << value2 << " = " << result << endl;
+
+    result = multiply(value1, value2);
+    cout << name << value1 <<" * " << value2 << " = " << result << endl;
+
+    result = divide(value1, value2);
+    cout << name << value1 << " / " << value2 << " = " result << endl;
+
+    return 0;
+}
